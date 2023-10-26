1d700c25b5bcd734372254e6695ca2cb61c0b04b
zohaibxJackie <zkhan52607@gmail.com>
2023-10-26 12:27:45 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("burger").addEventListener("click", () => {
+    document.getElementById("responsive-page-id").style.display = "block"
+})
+
+document.getElementById("return-btn").addEventListener("click", () => {
+    document.getElementById("responsive-page-id").style.display = "none"
+})
